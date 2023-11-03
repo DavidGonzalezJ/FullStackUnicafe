9cbf4c6a74c9b36b5c32b6b651b6408b947b0f5c
David <davidgonzalezj97@gmail.com>
2023-11-03 14:01:26 +0100

    Exercise 1.8 Refactoring Statistics in a new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import { useState } from 'react'
 
+//Button component
 const Button = ({eventHandler,text}) =>
   <button onClick={eventHandler}>{text}</button>
+
+//Statistics component
+const Statistics = ({good,neutral,bad}) => {
+
+  const average = () =>{ 
+    const total = good+bad+neutral
+    if (total === 0)
+      return 0
+    return (good - bad)/total
+  }
+
+  const positivePercentage = () => {
+    const total = good+bad+neutral
+    if (total === 0)
+      return 0
+    return good*100/total
+  }
+
+  return(
+    <>
+    <h1>Statistics</h1>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>All: {good + bad + neutral}</li>
+        <li>Average: {average()}</li>
+        <li>Positive: {positivePercentage()} %</li>
+      </ul>
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -21,34 +54,13 @@ const App = () => {
     setBad(bad+1)
   }
 
-  const average = () =>{ 
-    const total = good+bad+neutral
-    if (total === 0)
-      return 0
-    return (good - bad)/total
-  }
-
-  const positivePercentage = () => {
-    const total = good+bad+neutral
-    if (total === 0)
-      return 0
-    return good*100/total
-  }
   return (
     <div>
       <h1>Give feedback</h1>
       <Button eventHandler={giveGoodFeedback} text={'good'} />
       <Button eventHandler={giveNeutralFeedback} text={'neutral'} />
       <Button eventHandler={giveBadFeedback} text={'bad'} />
-      <h1>Statistics</h1>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>All: {good + bad + neutral}</li>
-        <li>Average: {average()}</li>
-        <li>Positive: {positivePercentage()} %</li>
-      </ul>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
